9e3364956294d8e447925e0eafdbe960ac920759
Ed Santos <edsantos2822@gmail.com>
2022-06-06 00:55:14 -0300

    test: implementando test 3

--- cypress/integration/my_google.spec.js
+++ cypress/integration/my_google.spec.js
@@ -3,10 +3,15 @@
 const HEADER = 'header';
 const MAIN = 'main';
 const MAIN_SECTION = 'main section';
+const FOOTER = 'footer';
+const FOOTER_SECTION = 'footer section';
+const FOOTER_LINKS = `${FOOTER_SECTION} div.footer-links`;
+const LINKS_FOR_GOOGLE = '#links-for-google'
+const LINKS_FOR_YOU = '#links-for-you'
 
 describe('My Google', () => {
   beforeEach(() => {
-    cy.visit('../../base_answer/')
+    cy.visit('./index.html')
   });
 
   describe('1 - Testando </ header>', () => {
@@ -34,6 +39,36 @@ describe('My Google', () => {
 
     it('A </ section> deve possuir dois elementos', () => {
       cy.get(MAIN_SECTION).children().should('have.length', 2);
+    });
+  });
+
+  describe('3 - Testando </ footer>', () => {
+    it('Crie um </ footer>', () => {
+      cy.get(FOOTER).should('exist');
+    });
+
+    it('O </ footer> deve possuir 1 elemento', () => {
+      cy.get(FOOTER).children().should('have.length', 1);
+    });
+
+    it('O </ footer> deve possuir uma </ section>', () => {
+      cy.get(FOOTER_SECTION).should('exist');
+    });
+
+    it('A </ section> deve possuir 2 elementos', () => {
+      cy.get(FOOTER_SECTION).children().should('have.length', 2);
+    });
+
+    it('O segundo elemento da </ section> deve possuir 2 elementos (este elemento deve possuir a classe "footer-links"', () => {
+      cy.get(FOOTER_LINKS).children().should('have.length', 2);
+    });
+
+    it('O primeiro elemento do filho do elemento com a classe "footer-links" deve possuir o atributo id="links-for-google" e 4 elementos', () => {
+        cy.get(LINKS_FOR_GOOGLE).children().should('have.length', 4);
+    });
+
+    it('O segundo elemento do filho do elemento com a classe "footer-links" deve possuir o atributo id="links-for-you" e 3 elementos', () => {
+      cy.get(LINKS_FOR_YOU).children().should('have.length', 3);
     });
   });
 });
